815f1dab0c49a90ac912ce8b52c868b7bc204983
Sterling Hanenkamp <sterling@hanenkamp.com>
2022-04-08 23:54:08 -0500

    move project code into its own module

--- pkg/rotate/rotate.go
+++ pkg/rotate/rotate.go
@@ -3,7 +3,6 @@ package rotate
 import (
 	"context"
 	"encoding/base64"
-	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -16,54 +15,6 @@ import (
 
 	"github.com/zostay/aws-github-rotate/pkg/config"
 )
-
-// Project is the compiled metadata about each project for which we manage the
-// secrets and the associated secret metadata.
-type Project struct {
-	*config.Project
-
-	SecretUpdatedAt time.Time // last update time of the github action secret
-
-	// We cache access key metadata to avoid making multiple calls to IAM that
-	// return the same information.
-
-	OldestKey  *iam.AccessKeyMetadata // the oldest IAM key metadata
-	NewestKey  *iam.AccessKeyMetadata // the newest IAM key metadata
-	keysCached bool                   // true after oldestKey/newestKey are set (possibly to nil)
-}
-
-// TouchGithub sets the SecretUpdatedAt time to right now.
-func (p *Project) TouchGithub() {
-	p.SecretUpdatedAt = time.Now()
-}
-
-// ClearAWS clears the oldest and newest key cache.
-func (p *Project) ClearAWSKeyCache() {
-	p.OldestKey = nil
-	p.NewestKey = nil
-	p.keysCached = false
-}
-
-var (
-	ErrNotCached = errors.New("AWS keys not cached")
-)
-
-// GetAWSCache returns the cached keys and nil if they are cached or two nils
-// and an error if they are not cached.
-func (p *Project) GetAWSKeyCache() (*iam.AccessKeyMetadata, *iam.AccessKeyMetadata, error) {
-	if p.keysCached {
-		return p.OldestKey, p.NewestKey, nil
-	} else {
-		return nil, nil, ErrNotCached
-	}
-}
-
-// SetAWSCache sets the AWS key cache to the given keys.
-func (p *Project) SetAWSKeyCache(o, n *iam.AccessKeyMetadata) {
-	p.OldestKey = o
-	p.NewestKey = n
-	p.keysCached = true
-}
 
 // Rotate is an object capable of rotating a bunch of configured AWS password
 // related to github objects and then update the related action secrets.
