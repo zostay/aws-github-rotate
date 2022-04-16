a4b10c6482393622a49d0f1c5c0512b0a858dce9
Sterling Hanenkamp <sterling@hanenkamp.com>
2022-04-15 22:25:10 -0500

    godoc

--- pkg/rotate/interface.go
+++ pkg/rotate/interface.go
@@ -7,16 +7,71 @@ import (
 	"github.com/zostay/aws-github-rotate/pkg/secret"
 )
 
+// Storage is the client interface implemented by storage client plugins. A
+// storage plugin must provide a means of checking the last time a given secret
+// was stored and a means of updating the stored password upon rotation.
 type Storage interface {
+	// Name must return the value in the configuration for reference to the
+	// adminstrator running the rotation service. This will be used in errors
+	// and logs.
 	Name() string
+
+	// LastSaved must return the timestampe the given secret was last updated in
+	// the storage or an error.
+	//
+	// The context provides a logger via context tools in the config package.
+	//
+	// The secret.Storage describes the information about the secret as it
+	// pertains to the storage client.
+	//
+	// The final string value is the individual key as a given secret might have
+	// multiple values. For example, a user might have a username and password
+	// that changes each time or it might have a an API key and a secret key.
+	// This method will be called for each.
 	LastSaved(context.Context, secret.Storage, string) (time.Time, error)
+
+	// SaveKeys will be called a single time for each rotation. It must peform
+	// storage of the secret following a fresh secret rotation or return an
+	// error.
+	//
+	// The context provides a logger via context tools in the config package.
+	//
+	// The secret.Storage describes information about the secret as it pertains
+	// to the storage client.
+	//
+	// The map is the set of values to be stored. This will already be remapped
+	// from the values output by the rotation client into the values configured
+	// for storage.
 	SaveKeys(context.Context, secret.Storage, secret.Map) error
 }
 
-type Storages []Storage
-
+// Client is the interface impelemnted by rotation plugins. These are plugins
+// responsible for performing the rotation of secrets.
 type Client interface {
+	// Name must return the value in the configuration for reference to the
+	// adminstrator running the rotation service. This will be used in errors
+	// and logs.
 	Name() string
+
+	// LastRotated must return the date of the most recent rotation of the given
+	// secret or an error.
+	//
+	// The context provides a logger via context tools in the config package.
+	//
+	// The secret.Info describes the secret to be rotated.
 	LastRotated(context.Context, secret.Info) (time.Time, error)
+
+	// RotateSecret must immediately rotate the secret and return a map
+	// containing all the new values. The keys returned should be carefully
+	// documented and be consistent so the adminsitrator running the service can
+	// remap them to storages as required using static names in the
+	// configuration. It is recommended that the names be the most natural names
+	// for the accounting system being rotated.
+	//
+	// If rotation cannot be performed, an error must be returned.
+	//
+	// The context provides a logger via context tools in the config package.
+	//
+	// The secret.Info describes the secret to be rotated.
 	RotateSecret(context.Context, secret.Info) (secret.Map, error)
 }
--- pkg/secret/interface.go
+++ pkg/secret/interface.go
@@ -1,19 +1,61 @@
+// Package secret defines some generic interfaces used to describe secrets in
+// different contexts. There are two contexts:
+//
+// * Info
+// * Storage
+//
+// The Info context is used to describe the secret for use with the rotation and
+// disablement plugin clients. This will describe the user or role or
+// host or whatever that owns the secret. This is the server side
+// description of the secret. There is only one Info context per secret.
+//
+// The Storage context is used to describe teh secret for use with the storage
+// plugin clients. This will describe the project, the service, the application,
+// or whatever that uses the secret to do something. This is the cilent side of
+// description of the secret. There can be zero or more Storage contexts per
+// secret.
 package secret
 
+// Cache each secret context has an associated cache, which allows the plugin
+// client to store any information that is expensive to caculate with the secret
+// context.
 type Cache interface {
+	// CacheSet stores a value in the secret cache.
 	CacheSet(any, any)
+
+	// CacheGet retrieves a value from the secret cache. It returns the value
+	// stored and a boolean value indicating whether any value was stored (that
+	// way, a nil value can be stored).
 	CacheGet(any) (any, bool)
+
+	// CacheClear delets a value from the secret cache.
 	CacheClear(any)
 }
 
+// Storage describes the secret from the client side for use with the associated
+// storage plugin client. It provides a Cache for storing data with the secret.
 type Storage interface {
 	Cache
+
+	// Name describing where to store this secret.
 	Name() string
 }
 
+// Info describes the secret for server side use with rotation or disablement
+// plugin clients. It provides a Cache for storing data with the secret.
 type Info interface {
 	Cache
+
+	// Name describing which secret account to rotation.
 	Name() string
 }
 
+// Map is the object used to contain a map of keys to secret values. Upon
+// rotation, the rotation plugin will return one of these objects containing new
+// the new secret values. It is recommended that the keys in such a case be the
+// same for every secret for that plugin client and be the most natural names
+// associated with each field for that secre value.
+//
+// These names may be remapped by the rotation business logic to provide
+// per-storage names.
 type Map map[string]string
